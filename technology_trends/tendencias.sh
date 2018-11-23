0cf0a8f539b00542ffdc6ffb6688ddf44677dcd4
Jonathan <jpradoar@gmail.com>
2018-11-23 19:39:58 -0300

    Update tendencias.sh

--- technology_trends/tendencias.sh
+++ technology_trends/tendencias.sh
@@ -3,11 +3,7 @@
 clear
 date=$(date)
 echo -e "================================= \n" $date "\n=================================\n " >> tendencias.txt
-
-#lista_tecnologias=("Linux" "Docker" "Kubernetes" )
 lista_tecnologias=("Linux" "Docker" "Kubernetes" "AWS" "Ansible" "Terraform" "Bash" "Python" "nosql" "Redis" "RabbitMQ" "Elasticsearch" "Prometheus" "Grafana" "Kibana" "InfluxDB" "GitLab" "GitHub" "Jenkins")
-
-
 for i in "${lista_tecnologias[@]}"
 do
 	sitios=(
@@ -25,23 +21,16 @@ do
 		echo $resultado; echo $resultado >> tendencias.txt
 	done
 done
-
-
 echo -e "\n================================= \n" ESTADISTICAS "\n=================================\n " >> tendencias.txt
-
 echo "<!DOCTYPE html><html><body><br><div align='center'><h1>Technologies popularity</h1><pre>" >index.html
 for z in "${lista_tecnologias[@]}"
 do
         cat tendencias.txt | sort -n | grep $z | cut -d'>' -f2 | awk '{sum += $1}END{print sum}' | sed 's/$/ '$z' /g' |tee -a tendencias.txt index.html
-#	cat tendencias.txt | sort -n | grep $z | cut -d'>' -f2 | awk '{sum += $1}END{print sum}' | sed 's/$/ '$z' /g' >> index.html 
 done
 echo "</pre><br><h1>Show in Google Trends</h1>" >> index.html
-
 for t in "${lista_tecnologias[@]}"
 do
         echo '<script type="text/javascript" src="https://ssl.gstatic.com/trends_nrtr/1644_RC01/embed_loader.js"></script> <script type="text/javascript"> trends.embed.renderExploreWidget("TIMESERIES", {"comparisonItem":[{"keyword":"'$t'","geo":"","time":"today 5-y"}],"category":0,"property":""}, {"exploreQuery":"date=today%205-y&q=Linux","guestPath":"https://trends.google.com:443/trends/embed/"}); </script> '>> index.html
 done
 echo "</div></body></html>" >> index.html
-
-#cat tendencias.txt
 cp -rp index.html /var/www/html/index.html
