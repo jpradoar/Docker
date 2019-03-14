fa118fe047ec70b7fb8a8a45f0d78891c5dcb026
Jonathan Prado <jpradoar@gmail.com>
2019-03-14 11:46:17 -0300

    update consumer data ;)

--- DolarArg/scripts/getdolar.sh
+++ DolarArg/scripts/getdolar.sh
@@ -8,4 +8,6 @@ while true; do
 	curl -H 'Content-Type: application/json' -XPOST http://elasticsearch:9200/dolar/status/?pretty=true -d '{"source": "dolarhoy.com", "price" : '$dolarhoy', "@timestamp" : "'$(date --utc +%FT%T.%3NZ)'"}'
 #	echo "dolarhoy"
 	sleep 10
+        bancogalicia=$(curl -s 'https://dolaraldia.com/bancosargentina/banco-galicia/' -H 'authority: www.preciodolar.com.ar' -H 'upgrade-insecure-requests: 1' -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36' -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' |grep CotizacionBanco |grep "Banco Galicia"|cut -d"/" -f4- |cut -d"<" -f1)   &&  echo "'{"source": "BancoGalicia", "price" : '$bancogalicia', "@timestamp" : "'$(date --utc +%FT%T.%3NZ)'"}'"
+       
 done
